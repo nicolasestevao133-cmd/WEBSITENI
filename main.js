77edcf1aa3f1485519299fd4429a9c1cf68372b3
Nick big <nicolasestevao133@gmail.com>
2026-09-17 08:44:32 -0300

    trabis

---
+++ main.js
@@ -0,0 +1,23 @@
+/*Tipos de variáveis
+var -> escopo global
+let -> escopo local
+const -> escopo flex, valor será o mesmo até o fim do programa
+*/
+
+/*Criando variável*/
+var valorUm = 3
+var valorDois = 19
+
+/*
+espremedor (fruta){
+    suco
+}
+*/
+
+/*função*/
+function somar(a,b){
+    let c = a+b
+    return (c)
+}
+
+console.log(somar(valorUm,valorDois))
